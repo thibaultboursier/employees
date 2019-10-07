8e493f359dbe9ab2448ab164038c674144985567
Thibault Boursier <boursier.thibault@gmail.com>
2019-10-07 23:45:38 +0200

    wip: display user informations

--- pages/employees/[slug].js
+++ pages/employees/[slug].js
@@ -1,5 +1,9 @@
 import fetch from "isomorphic-fetch";
+
 import Head from "next/head";
+
+import { Grid, Paper, Typography, Avatar } from "@material-ui/core";
+
 import { getIdFromSlug } from "../../utils/slug";
 import { API } from "../../config";
 import Layout from "../../components/Layout";
@@ -14,6 +18,11 @@ const Employee = ({ data: employee }) => {
           Employees - {employee.first_name} {employee.last_name}
         </title>
       </Head>
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+          <Paper></Paper>
+        </Grid>
+      </Grid>
     </Layout>
   );
 };
